4283dade658353e78758750767b8c1c6fcc8b909
josh <josh@jhmail.co>
2025-08-08 15:40:50 -0600

    Cleaned up wfClient

--- src/workfront-ui-1/actions/utils/wfClient.js
+++ src/workfront-ui-1/actions/utils/wfClient.js
@@ -63,59 +63,6 @@ class WorkfrontServiceClient {
       return totalResponse;
     }
   }
-
-  async get(obj, params = {}) {
-    const response = await this.axiosInstance.get(`/${obj.objCode}/${obj.ID}`, { params: params });
-    return response.data.data;
-  }
-
-  async query(obj, params = {}) {
-    const response = await this.axiosInstance.get(`/${obj.objCode}`, { params: params });
-    return response.data.data;
-  }
-
-  async post(obj, body, params = {}) {
-    const response = await this.axiosInstance.post(`/${obj.objCode}`, body, { params: params });
-    return response.data.data;
-  }
-
-  async put(obj, body, params = {}) {
-    const response = await this.axiosInstance.put(`/${obj.objCode}/${obj.ID}`, body, { params: params });
-    return response.data.data;
-  }
-
-  async delete(objCode, objID) {
-    const response = await this.axiosInstance.delete(`/${objCode}/${objID}`);
-    return response.data.data;
-  }
-
-  async search(objCode, filters, params = {}) {
-    if (!filters['$$LIMIT'] || filters['$$LIMIT'] > 2000) {
-      filters['$$LIMIT'] = 2000;
-    }
-    const countResponse = await this.axiosInstance.get(`/${objCode}/count`, { params: filters });
-    const countTotal = countResponse.data;
-    let totalResponse = [];
-    if (countTotal.data.count > 2000) {
-      let retrieved = 0;
-      while (retrieved < countTotal.data.count) {
-        filters['$$FIRST'] = retrieved;
-        const paginatedResponse = await this.axiosInstance.get(
-          `/${objCode}/search`,
-          { params: Object.assign(filters, params) }
-        );
-        totalResponse = totalResponse.concat(paginatedResponse.data.data);
-        retrieved = totalResponse.length;
-      }
-    } else {
-      const resp = await this.axiosInstance.get(
-        `/${objCode}/search`,
-        { params: Object.assign(filters, params),  }
-      );
-      totalResponse = resp.data.data;
-    }
-    return totalResponse;
-  }
 }
 
 module.exports = { WorkfrontServiceClient };
